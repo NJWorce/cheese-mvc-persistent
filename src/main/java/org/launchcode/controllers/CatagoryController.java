a0ec4856ad46d6faceb28f0b8e890fcc58fccf8f
Nathaniel Worcester <nathaniel.worcester@gmail.com>
2019-02-08 20:50:35 -0600

    Completed the first phase including the add category form, view category, cat controller, ect.

--- src/main/java/org/launchcode/controllers/CatagoryController.java
+++ src/main/java/org/launchcode/controllers/CatagoryController.java
@@ -5,8 +5,12 @@ import org.launchcode.models.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -21,11 +25,26 @@ public class CatagoryController {
         Iterable<Category> categories = categoryDao.findAll();
 
         model.addAttribute("categories", categories);
-        model.addAttribute("title", "Categories in Which Cheese Exists in an Abstract Manner");
+        model.addAttribute("title", "Categories of Cheese");
         return "category/index" ;
-
-
     }
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String add(Model model){
+
+        model.addAttribute(new Category());
+        model.addAttribute("title" , "Add Category");
+
+        return "category/add";
+    }
+    @RequestMapping(value="add", method= RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Category category, Errors errors){
+        if (errors.hasErrors()){
+            return "category/add";
+        }
+        categoryDao.save(category);
+        return "redirect:";
+    }
+
 
 
 
